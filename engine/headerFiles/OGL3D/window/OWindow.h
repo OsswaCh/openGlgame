16d42b7efd68c3b3e275f77aace7926c930f4804
ousswa <ousswachouchane@aucegypt.edu>
2024-01-05 23:05:43 +0200

    finished window class + creation of game class

--- engine/headerFiles/OGL3D/window/OWindow.h
+++ engine/headerFiles/OGL3D/window/OWindow.h
@@ -4,6 +4,8 @@ class OWindow
 {
 
 private:
+    void *m_handle = nullptr; // void pointer is used to hold the address of a memory location without specifying the type of data that is being pointed to.
+
 public:
     OWindow();
     ~OWindow();
--- engine/sourceFiles/OGL3D/window/OWindow.cpp
+++ engine/sourceFiles/OGL3D/window/OWindow.cpp
@@ -1,13 +1,33 @@
 #include <OWindow.h>
 #include <windows.h>
+#include <assert.h>
 
 OWindow::OWindow()
 {
 
-    WNDCLASSEX wc = {};             // This line initializes an instance of the WNDCLASSEX structure named wc to all zeros.
-    wc.cbSize = sizeof(WNDCLASSEX); // sets the cbSize member of the WNDCLASSEX structure wc to the size of the WNDCLASSEX structure itself
+    WNDCLASSEX wc = {};               // This line initializes an instance of the WNDCLASSEX structure named wc to all zeros.
+    wc.cbSize = sizeof(WNDCLASSEX);   // sets the cbSize member of the WNDCLASSEX structure wc to the size of the WNDCLASSEX structure itself
+    wc.lpszClassName = L"OG3DWindow"; // The lpszClassName member is where you specify the unique name by which the window class will be known.
+    wc.lpfnWndProc = DefWindowProc;   // The lpfnWndProc member is a pointer to the window procedure that you want to associate with this window class.
+
+    // we need to register this class in order to let the operating system know about it and use it
+
+    assert(RegisterClassEx(&wc)); // The RegisterClassEx function takes a pointer to a WNDCLASSEX structure and a handle to the application instance as its parameters. It returns a class atom that uniquely identifies the window class. If the function fails, it returns zero.
+    // The assert macro is used to check for errors during runtime. If the expression inside the assert macro evaluates to false, the program will terminate and an error message will be displayed.
+
+    RECT rc = {0, 0, 1024, 768};                                           // The RECT structure defines the coordinates of the upper-left and lower-right corners of a rectangle.
+    AdjustWindowRect(&rc, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, false); // The AdjustWindowRect function takes a pointer to a RECT structure and a window style as its parameters. It returns a RECT structure with the coordinates of the window adjusted to account for the window style.
+
+    m_handle = CreateWindowEx(NULL, L"OG3DWindow3", L"PardCode | openGL 3d game", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, NULL, NULL);
+
+    assert(m_handle);
+
+    ShowWindow((HWND)m_handle, SW_SHOW); // The ShowWindow function takes a handle to a window and a show command as its parameters. It returns a Boolean value indicating success or failure. The show command can be one of the following values:
+    UpdateWindow((HWND)m_handle);
 }
 
 OWindow::~OWindow()
 {
+
+    DestroyWindow((HWND)m_handle);
 }

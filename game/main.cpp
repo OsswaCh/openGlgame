335b9502325f72e8eda38c226643e047c0211087
ousswa <ousswachouchane@aucegypt.edu>
2024-01-06 02:31:57 +0200

    fixed the inclusion (fix windows.cpp plz)

--- game/main.cpp
+++ game/main.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+
+#include "D:/code/openGLgame/engine/headerFiles/OGL3D/game/OGame.h"
 #include "D:/code/openGLgame/engine/sourceFiles/OGL3D/game/OGame.cpp"
+#include "D:/code/openGLgame/engine/sourceFiles/OGL3D/window/OWindow.cpp"
+
 int main()
 {
 
